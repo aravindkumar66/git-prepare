d55f3821b5f89b694bb322953e059ce8166ca227
aravind kumar <aravindkumarreddy66@gmail.com>
2025-08-23 11:18:05 +0530

    functions

--- 05-arrary.sh
+++ 05-arrary.sh
@@ -4,4 +4,4 @@ FRUITS=("Apple" "KIWI" "MANGO" "GRAPS" "HOMOGRANITE")
 
 echo "First fruit is: ${FRUITS[0]}"
 
-echo "First fruit is: ${FRUITS[1]}"
+echo "Second fruit is: ${FRUITS[1]}"
---
+++ 06-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Please enter user name here: "
+
+read USERNAME
+
+echo "entered user name is: $USERNAME"
