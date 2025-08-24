4daaadb8f08950f8720b752f5907452f174e7b4a
aravind kumar <aravindkumarreddy66@gmail.com>
2025-08-24 11:21:50 +0530

    functions

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,9 +1,10 @@
  #!/bin/bash
 
- NUMBER=$
+ NUMBER=$1
 
- if ( NUMBER -gt 20)
+ if ( NUMBER -gt 20 )
     then 
         echo ("the number $NUMBER is greater than 20")
     else
         echo ("the number $NUMBER is not greater than 20")
+ fi
