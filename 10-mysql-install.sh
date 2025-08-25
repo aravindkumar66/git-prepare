ceea0d4a0ab65a089aa9af06c03fbe2704904215
aravind kumar <aravindkumarreddy66@gmail.com>
2025-08-25 14:23:09 +0530

    functions

--- 10-mysql-install.sh
+++ 10-mysql-install.sh
@@ -1,5 +1,18 @@
  #!/bin/bash
 
- USER=$(id -u)
+ dnf list installed mysql
+ if [ $? -ne 0 ]
+ then
+    echo "mysql is not installed, going to install it"
+    dnf install -y mysql
 
- if [ $USER -ne 0 ]
+    if [ $? -ne 0]
+    then
+        echo "mysql installation failed, please check"
+        exit 1
+    else
+        echo "mysql installation is sucess"
+    fi
+ else
+    echo "mysql is already installed, nothind to do"
+ fi
