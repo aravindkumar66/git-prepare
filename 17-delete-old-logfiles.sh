3e43e1b37809fb38f194795db0546f6360d0cca9
aravind kumar <aravindkumarreddy66@gmail.com>
2025-09-01 19:18:13 +0530

    functions

---
+++ 17-delete-old-logfiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+Source= /home/ec2-user/logs 
+
+
+if [-d Source]
+then 
+    echo "$Source files is exisits"
+    
+else
+    echo "$Source file is not exists"
+    exit 1
+fi
+FILES = $(find ${Source} -name "*.logs" -mtime +30)
+echo "fiels: $FILES"
