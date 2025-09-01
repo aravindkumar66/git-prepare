99f8ab46175b383827b20a0a377690d8b4bf1c27
aravind kumar <aravindkumarreddy66@gmail.com>
2025-09-01 17:44:01 +0530

    functions

--- 16-set.sh
+++ 16-set.sh
@@ -1,6 +1,10 @@
  #!/bin/bash
 
- set -ex #exit automatically if we get error
+ failuer{
+    echo "failed at: $1 $2"
+ }
+ trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR
+# set -ex #exit automatically if we get error #set -ex for debug
  echo "Hello World"
  echoo "Welcome to java city"
  echo "learning java language error in above"
