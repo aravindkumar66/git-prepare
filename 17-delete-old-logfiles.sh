5c333118e106803195c6d037d6b14c101cb48038
aravind kumar <aravindkumarreddy66@gmail.com>
2025-09-03 11:59:00 +0530

    functions

--- 17-delete-old-logfiles.sh
+++ 17-delete-old-logfiles.sh
@@ -8,7 +8,7 @@ if [ -d $Source ]
 then 
     echo "$Source directory is exisits"
     
-else
+else 
     echo "$Source directory is not exists"
     exit 1
 fi
@@ -22,13 +22,3 @@ do
 
 done <<< $FILES
 
-# if [ -d $SOURCE_DIR ]
-# then
-#     echo -e "$SOURCE_DIR $G Exists $N"
-# else
-#     echo -e "$SOURCE_DIR $R does not exist $N"
-#     exit 1
-# fi
-
-# FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
-# echo "Files: $FILES"
---
+++ 18-backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+Source_Dir = $s1
+Dest_Dir = $s2
+Days = ${3:-14}
+
+USAGE(){
+
+    echo "USAGE: 18-backup.sh <source> <destination> <days(optional)>"
+} 
+if [ $# -lt 2]
+then
+    USGE
+fi
+
+
+
+# if [ $# -lt 2]
+# then
+#     echo "Source_Dir and Dest_Dir are exist"
+# else
+#     cho "Source_Dir and Dest_Dir are does not exist"
+# fi
